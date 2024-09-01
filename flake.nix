e63cfb7dd0596c5b520fb6823cc881cc72753a04
ge-garcia <gabrielgarciacs@proton.me>
2024-09-01 11:03:42 -0400

    add correct go version, (21)

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "A Nix-flake-based Go 1.17 development environment";
+  description = "Environment for tecla";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
   outputs = { self, nixpkgs }:
     let
-      goVersion = 20; # Change this to update the whole stack
+      goVersion = 21; # Change this to update the whole stack
       overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
       supportedSystems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
